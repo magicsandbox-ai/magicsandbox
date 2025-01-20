8bd152badff7a6f0bfa1e0b80137f8fcf849a5c5
magicsandbox-ai <admin@magicsandbox.ai>
2025-01-20 17:58:36 -0500

    findapp progress

--- functions/findApp/findApp.py
+++ functions/findApp/findApp.py
@@ -14,12 +14,17 @@ import boto3
 import msgpack
 from fastapi import Response
 import uuid
+import time
 from .prompts import get_app_descriptions_from_input_prompt
 
 '''
-create s3 endpoint bucket
+more sophisticated handling of input_ids - make sure memory doesn't grow too large
 adjust this to enable functions as well
 variable cost for ratings?
+
+issues with scaling this horizontally:
+ - inputId not shared across instances
+ - instances will overwrite each other when persisting embeddings / not share updates
 '''
 
 class Rating(BaseModel):
@@ -56,6 +61,9 @@ class AppData:
         self.embedder = SentenceTransformer(os.getenv('EMBEDDING_MODEL'))
         self.s3 = boto3.client('s3')  # Create S3 client once
         self.init_app_data()
+        self.input_ids = {}
+        # if os.getenv('NODE_ENV') == 'production': #todo
+        #     asyncio.create_task(self.schedule_sync_app_data())
         asyncio.create_task(self.schedule_sync_app_data())
 
     async def schedule_sync_app_data(self):
@@ -81,6 +89,7 @@ class AppData:
             app_data = response.json()
         self.init_db(app_data)
         self.persist_app_embeddings()
+        self.input_ids = {k: v for k, v in self.input_ids.items() if v['ts'] > time.time() - 60 * 60 * 3}
 
     def get_url_and_headers(self):
         if os.getenv('NODE_ENV') == 'production':
@@ -98,7 +107,7 @@ class AppData:
         cur.execute('DROP TABLE IF EXISTS _app_data')
         cur.execute('''
             CREATE TABLE _app_data (
-                id TEXT PRIMARY KEY,
+                id TEXT COLLATE NOCASE PRIMARY KEY,
                 author TEXT,
                 name TEXT,
                 version TEXT,
@@ -139,36 +148,47 @@ class AppData:
         cur.execute('''
             CREATE TABLE app_data AS
             SELECT *
-              , row_number() over (partition by author, name order by major desc, minor desc, patch desc) = 1 as latest
+                , author || '.' || name COLLATE NOCASE as author_name
+                , row_number() over (partition by author, name order by major desc, minor desc, patch desc) = 1 as latest
             FROM _app_data
             WHERE kind = 'app' 
               and status = 'active'
               and type != 'assistant'
-                         ''')
+        ''')
+        cur.execute('CREATE INDEX idx_author_name ON app_data(author_name)')
         self.con.commit()
 
     def init_app_embeddings(self):
-        response = self.s3.get_object(Bucket=os.getenv('S3_ENDPOINT_BUCKET'), Key='findApp/app_embeddings.msgpack')
-        app_embeddings = msgpack.unpackb(response['Body'].read())
-        self.app_embeddings = {
-            'apps': app_embeddings['apps'],
-            'embeddings': torch.tensor(app_embeddings['embeddings'])
-        }
+        try:
+            response = self.s3.get_object(Bucket=os.getenv('S3_ENDPOINT_BUCKET'), Key='findApp/app_embeddings.msgpack')
+            app_embeddings = msgpack.unpackb(response['Body'].read())
+            self.app_embeddings = {
+                'apps': app_embeddings['apps'],
+                'apps_to_ix': {app: ix for ix, app in enumerate(app_embeddings['apps'])},
+                'embeddings': torch.tensor(app_embeddings['embeddings'])
+            }
+        except self.s3.exceptions.NoSuchKey: #todo remove this
+            self.app_embeddings = {
+                'apps': [],
+                'apps_to_ix': {},
+                'embeddings': torch.empty((0, self.embedder.get_sentence_embedding_dimension()))
+            }
         cur = self.con.cursor()
         cur.execute('SELECT id, description, name FROM app_data')
         self.add_app_embeddings(cur.fetchall())
 
     def add_app_embeddings(self, app_data):
-        apps_with_embeddings = set(self.app_embeddings['apps'])
         apps_to_embed = []
         sentences = []
         for app in app_data:
-            if app[0] not in apps_with_embeddings:
+            if app[0] not in self.app_embeddings['apps_to_ix']:
                 apps_to_embed.append(app[0])
                 sentences.append(app[1] or app[2])
+        new_apps_to_ix = {app: ix + len(self.app_embeddings['apps']) for ix, app in enumerate(apps_to_embed)}
         new_embeddings = self.embed(sentences)
         self.app_embeddings = {
             'apps': self.app_embeddings['apps'] + apps_to_embed,
+            'apps_to_ix': self.app_embeddings['apps_to_ix'] | new_apps_to_ix,
             'embeddings': torch.cat([self.app_embeddings['embeddings'], new_embeddings])
         }
 
@@ -220,6 +240,8 @@ class AppData:
         
     async def find_app(self, args: FindAppArgs):
         input_embedding = await self.embed_input(args.input)
+        input_id = str(uuid.uuid4())
+        self.input_ids[input_id] = {'embedding': input_embedding, 'ts': time.time()}
         valid_apps = self.get_valid_apps(args.maxCost, args.apps)
         valid_mask = torch.tensor([id in valid_apps for id in self.app_embeddings['apps']])
         original_indices = torch.where(valid_mask)[0]
@@ -240,7 +262,7 @@ class AppData:
         response = {
             'inputEmbedding': input_embedding.tolist(),
             'apps': apps,
-            'inputId': str(uuid.uuid4())
+            'inputId': input_id
         }
         return Response(content=msgpack.packb(response))
 
@@ -271,37 +293,40 @@ class AppData:
         return self.embedder.encode(sentences, normalize_embeddings=True, convert_to_tensor=True)
 
     def get_valid_apps(self, maxCost: float, apps: list[str]):
-        # todo case sensitivity, what if apps is empty, what if apps doesn't specify version
-        # todo include apps here?
+        # todo app may not have an embedding
         cur = self.con.cursor()
-        cur.execute('''
-            SELECT id, minCost
-            FROM app_data
-            WHERE id IN (?)
-              OR (latest AND minCost <= ?)
-        ''', (apps, maxCost))
+        query = 'SELECT id, minCost FROM app_data WHERE (latest AND minCost <= :maxCost)'
+        if len(apps) == 0:
+            cur.execute(query, {'maxCost': maxCost})
+        elif len(apps) == 1:
+            cur.execute(query + ''' OR id = :app OR author_name = :app''', {'maxCost': maxCost, 'app': apps[0]})
+        else:
+            cur.execute('CREATE TEMP TABLE requested_apps (app TEXT)')
+            cur.executemany('INSERT INTO requested_apps VALUES (?)', [(app,) for app in apps])
+            cur.execute(query + ''' OR id IN (SELECT app FROM requested_apps)
+                OR author_name IN (SELECT app FROM requested_apps)''', 
+                {'maxCost': maxCost})
         return {row[0]: row[1] for row in cur.fetchall()}
 
     def rate_app(self, rating: Rating):
-        input_embedding = self.get_input_embedding(rating.inputId)
-        app_embedding = self.get_app_embedding(rating.app)
-        app_embedding_norm = app_embedding.norm()
-        rating = min(max(rating.rating, -.1), .5)
+        input_embedding = self.input_ids.pop(rating.inputId, None)
+        if not input_embedding:
+            print(f'inputId {rating.inputId} not found')
+            return
+        input_embedding = input_embedding['embedding']
+        ix = self.app_embeddings['apps_to_ix'].get(rating.app, None)
+        if not ix:
+            print(f'app {rating.app} not found')
+            return
+        app_embedding = self.app_embeddings['embeddings'][ix]
         learning_rate = .05
-        # todo calculate desired norm separately
-        self.set_app_embedding(
-            rating.app, 
-            app_embedding + learning_rate * rating * input_embedding / app_embedding_norm
-            )
-
-    def get_input_embedding(self, inputId: str):
-        pass
-
-    def get_app_embedding(self, app: str):
-        pass
-        
-    def set_app_embedding(self, app: str, embedding: torch.Tensor):
-        pass
+        rating = min(max(rating.rating, -1), 1)
+        # norm update should be multiplicative, diminishing updates, and symmetric around 1
+        app_embedding_norm = app_embedding.norm()
+        update_factor = torch.exp(learning_rate * rating / (1 + torch.abs(app_embedding_norm.log())))
+        new_app_embedding_norm = min(max(app_embedding_norm * update_factor, 0.2), 5)
+        new_app_embedding = app_embedding + learning_rate * rating * input_embedding
+        self.app_embeddings['embeddings'][ix] = new_app_embedding / new_app_embedding.norm() * new_app_embedding_norm
 
 
 app_data = AppData()
