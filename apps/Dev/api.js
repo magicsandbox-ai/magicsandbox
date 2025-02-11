e3b39588eecd93b689e65ef41321eead700157cd
magicsandbox-ai <admin@magicsandbox.ai>
2025-02-11 14:03:10 -0500

    dev prompt and api progress

--- apps/Dev/api.js
+++ apps/Dev/api.js
@@ -5,16 +5,58 @@ import { getHeadings } from "@magicsandbox.ai/docs";
 
 function updateFiles(privateApi, updateString) {
   const newFiles = { ...privateApi.files };
-  // const updatedFiles = tagParser(updateString); //todo find/replace
   let invalidUpdateString = false;
   const updatedFiles = new Set();
-  for (const [filename, fileUpdateString] of Object.entries(
-    tagParser(updateString),
+  for (const { tag: filename, content: fileUpdateString } of tagParser(
+    updateString,
   )) {
-    if (filename === undefined && fileUpdateString) {
-      invalidUpdateString = true;
+    if (filename === undefined) {
+      if (fileUpdateString.trim() !== "") {
+        invalidUpdateString = true;
+      }
+      continue;
     }
-    newFiles[filename] = code;
+    updatedFiles.add(filename);
+    if (!fileUpdateString.trim().startsWith("<find>")) {
+      //update the whole file
+      //we need to look specifically for <find> rather than use tagParser because the file might be HTML or JSX and the tags are false positives
+      newFiles[filename] = fileUpdateString;
+    } else {
+      let find;
+      let invalidFileUpdateString = false;
+      for (const { tag, content } of tagParser(fileUpdateString)) {
+        if (tag === undefined) {
+          if (content.trim() !== "") {
+            invalidFileUpdateString = true;
+          }
+          continue;
+        }
+        if (tag === "find") {
+          if (find) {
+            console.warn("Consecutive <find> tag:", content);
+          }
+          find = content;
+        } else if (tag === "replace") {
+          if (find) {
+            newFiles[filename] = newFiles[filename].replace(find, content);
+            find = null;
+          } else {
+            console.warn("<replace> tag without <find> tag:", content);
+          }
+        }
+      }
+      if (find) {
+        console.warn("<find> tag without <replace> tag:", find);
+      }
+      if (invalidFileUpdateString) {
+        console.warn(
+          "When using <find> and <replace> tags, anything outside of a tag is ignored",
+        );
+      }
+    }
+  }
+  if (invalidUpdateString) {
+    console.warn("Anything in the updateString outside of a tag is ignored");
   }
   privateApi.setFiles(newFiles);
   //merges are the original files (kind of poorly named), so we want to keep any outstanding original files rather than overwrite them
--- apps/Dev/prompt.js
+++ apps/Dev/prompt.js
@@ -9,23 +9,42 @@ todos:
 - special instructions for init?
 */
 
-function prompt({ context, summarizedContext } = {}) {
+function prompt({ init, context, summarizedContext } = {}) {
   return `# magicsandbox.Dev
 
-magicsandbox.Dev is an App that enables developing, previewing, and publishing Magic Sandbox Apps in the browser.
+magicsandbox.Dev enables developing, previewing, and publishing Magic Sandbox Apps in the browser. By default, an App is built using the following files:
 
-- React
-- import anything
-- Tailwind
-- sandbox functions
+## magic.json
 
-## Context
+The App is configured by \`magic.json\`, which can be a JSON or JSON5 file. The complete configuration details are available using \`app.api.advancedDocs()\`. If the user is asking about \`magic.json\` specifically or if you suspect the user's request could be solved by updating \`magic.json\`, first use \`app.api.advancedDocs()\` to review the complete configuration details. For the most part, though, you should use the default values.
 
-The user is editing the below files.${
-    summarizedContext
-      ? `For brevity, files may be excluded (indicated by "...") or summarized. When summarized, individual blocks of code may be truncated (indicated by "...").`
-      : ""
-  }
+## index.js
+
+JavaScript file that's used as the entrypoint for the App. You can do the following:
+
+- Import npm packages: \`import React from "react";\`
+- Create other files and import them: \`import { myFunction } from "./utils.js";\`
+- Use Tailwind for styling
+- Use the Sandbox functions \`requestFunction\`, \`requestPutData\`, etc.
+
+## index.html
+
+Defaults to \`<div id="root"></div>\` if not provided.
+
+## index.css
+
+Defaults to \`@tailwind base; @tailwind components; @tailwind utilities;\` if not provided.
+
+${
+  init
+    ? `Below are sample files you can use as a template to create an App. Update \`magic.json\` and set relevant values for \`name\` and \`description\` based on the user's request. Unless the user requested otherwise or you feel it's inappropriate for the user's request, use \`index.js\` to create a React app and use Tailwind for styling. If using React and Tailwind, you likely don't need to update \`index.html\` or \`index.css\`.`
+    : `The user is editing the below files. Respect the existing code conventions and libraries used when updating the files.${
+        summarizedContext
+          ? ` For brevity, files may be excluded (indicated by "...") or summarized. When summarized, individual blocks of code may be truncated (indicated by "...").`
+          : ""
+      }`
+}
+
 ${context}
 
 ## API
@@ -34,13 +53,13 @@ ${context}
 
 ~~~javascript
 app.api.updateFiles(\`\`\`<index.js>
-<search>
+<find>
   return (
     <div className="flex h-screen items-center justify-center">
       Hello, world!
     </div>
   );
-</search>
+</find>
 <replace>
   return (
     <div className="flex h-screen items-center justify-center font-bold">
@@ -76,13 +95,14 @@ Logs advanced documentation that you can reference in your next message. The adv
 
 ## Instructions
 
-- If the user is asking a question about the code that you can answer, answer it and don't run any scripts.${
+- If the user is asking a question about the code that you can answer, answer it and don't run any scripts.
+- Only use \`app.api.updateFiles\` if it's clear the user is expecting you to update the files. If the user is just asking a question, answer it and ask the user if they want you to update the files.${
     summarizedContext
       ? `
-- Use \`app.api.additionalContext\` if the user is asking a question about the code but you can't answer it because the relevant file or snippet is not included in the context.`
+- Use \`app.api.additionalContext\` if the user is asking a question about the code that you can't answer because the relevant file or snippet is not included in the context.`
       : ""
   }
-- Use \`app.api.advancedDocs\` if the user asks a question related to Magic Sandbox or magicsandbox.Dev that you can't answer with the information you have available.
+- Use \`app.api.advancedDocs\` if the user asks a question related to Magic Sandbox, \`magic.json\`, magicsandbox.Dev, or updating already published Apps that you can't answer with the information you have available.
 `;
 }
 
