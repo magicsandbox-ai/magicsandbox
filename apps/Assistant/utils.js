7711a537309bbe0ba4f2821d8191e60831932c3f
magicsandbox-ai <admin@magicsandbox.ai>
2025-02-21 09:20:57 -0500

    kill @utils take 2

---
+++ apps/Assistant/utils.js
@@ -0,0 +1,12 @@
+const dollarFormatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 3,
+});
+
+function formatAsDollars(amount) {
+  return dollarFormatter.format(amount);
+}
+
+export { formatAsDollars };
