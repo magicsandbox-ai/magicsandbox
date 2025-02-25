33ab01c49686e28de6002830e85fcceca793fa05
magicsandbox-ai <admin@magicsandbox.ai>
2025-02-25 17:23:57 -0500

    working on prosemirror diff plugin but too hard, just going to prevent editing

--- apps/Notes/Note2.js
+++ apps/Notes/Note2.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { EditorState } from "prosemirror-state";
+import { EditorState, Plugin, PluginKey } from "prosemirror-state";
+import { Decoration, DecorationSet } from "prosemirror-view";
 import { Slice } from "prosemirror-model";
 import {
   schema,
@@ -11,8 +12,8 @@ import {
   ProseMirror,
   ProseMirrorDoc,
   reactKeys,
-  useEditorEffect,
 } from "@handlewithcare/react-prosemirror";
+import { diffLines } from "diff";
 
 /*
 documents serialized to markdown
@@ -25,50 +26,103 @@ somehow display those diffs
     transaction vs transformation?
   add marks?
   widget? nodeviews? schema?
+  maybe a highlight?
 
 
 ctrl+enter to escape code. or escape then enter
   or keep space at bottom always? or handle down arrow?
 handle tabs in lists to indent
 deleting input rules?
+menu? 
+  Make sure you load style/prosemirror.css as a stylesheet when using prosemirror-view
 */
 
-const originalContent = "### Hello World!";
-const newContent = "## Goodbye!";
+const originalContent = "### Hello World!\n\n test";
+const newContent = "## Goodbye!\n\n test";
+const diff = diffLines(originalContent, newContent);
 
-function EditorEffectHandler() {
-  useEditorEffect((view) => {
-    console.log(defaultMarkdownSerializer.serialize(view.state.doc));
+/*
+how to turn on and off? reconfigure?
+*/
+
+function diffPlugin() {
+  return new Plugin({
+    key: new PluginKey("diff"),
+    state: {
+      init(config, state) {
+        return {
+          original: defaultMarkdownSerializer.serialize(state.doc),
+          diff: [],
+        };
+      },
+      apply(tr, value, oldState, newState) {
+        return {
+          original: value.original,
+          diff: diffLines(
+            value.original,
+            defaultMarkdownSerializer.serialize(newState.doc),
+          ),
+        };
+      },
+    },
+    props: {
+      decorations(state) {
+        return DecorationSet.create(state.doc, [
+          Decoration.inline(0, state.doc.content.size, {
+            style: "color: purple",
+          }),
+        ]);
+      },
+    },
   });
-  return null;
 }
+
+const plugins = [...exampleSetup({ schema, menuBar: false }), reactKeys()];
 
 function Note() {
   const [editorState, setEditorState] = useState(
     EditorState.create({
       doc: defaultMarkdownParser.parse(originalContent),
-      plugins: [...exampleSetup({ schema, menuBar: false }), reactKeys()],
+      plugins,
     }),
   );
+  //const [editable, setEditable] = useState(true);
 
   function update() {
-    const newDoc = defaultMarkdownParser.parse(newContent);
+    setEditorState(
+      editorState.reconfigure({ plugins: [...plugins, diffPlugin()] }),
+    );
+    const diffedContent = diff
+      .map((change) => {
+        const symbol = change.added ? "+" : change.removed ? "-" : "";
+        return `${symbol}${change.value}`;
+      })
+      .join("\n");
+    const newDoc = defaultMarkdownParser.parse(diffedContent);
     const tr = editorState.tr.replace(
       0,
       editorState.doc.content.size,
       new Slice(newDoc.content, 0, 0),
     );
     setEditorState((s) => s.apply(tr));
+    //setEditable(false);
   }
 
   return (
     <ProseMirror
       state={editorState}
       dispatchTransaction={(tr) => {
-        setEditorState((s) => s.apply(tr));
+        const newState = editorState.apply(tr);
+        setEditorState(newState);
+        const diffKey = new PluginKey("diff");
+        const { diff = [] } = diffKey.getState(newState) || {};
+        if (diff.length === 0) {
+          setEditorState(newState.reconfigure({ plugins }));
+        }
+        //console.log(defaultMarkdownSerializer.serialize(newState.doc));
       }}
+      //editable={() => editable}
     >
-      <EditorEffectHandler />
       <Button onClick={update} />
       <ProseMirrorDoc />
     </ProseMirror>
