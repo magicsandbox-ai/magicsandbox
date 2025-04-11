ff7e5e1d05e91f46545a15521895e48239ced84c
magicsandbox-ai <admin@magicsandbox.ai>
2025-04-11 08:33:43 -0400

    python publishing

--- scripts/publishPythonPackage.js
+++ scripts/publishPythonPackage.js
@@ -5,7 +5,8 @@ import { execSync } from "child_process";
 npm run publish-python:prod magicsandbox_streaming
 */
 
-const path = `packages/python/${process.argv[2]}`;
+const packageName = process.argv[2];
+const path = `packages/python/${packageName}`;
 
 prePublish();
 
@@ -16,9 +17,13 @@ const version = execSync(
   .toString()
   .trim();
 
-const tag = `${path}@${version}`;
+const tag = `${packageName}@${version}`;
 
-execSync("pipenv shell && flit build", { cwd: path, stdio: "inherit" });
-execSync("pipenv run flit publish", { cwd: path, stdio: "inherit" });
+execSync(
+  `pipenv shell && flit -f ${path}/pyproject.toml build && flit -f ${path}/pyproject.toml publish`,
+  {
+    stdio: "inherit",
+  },
+);
 
 postPublish(tag);
